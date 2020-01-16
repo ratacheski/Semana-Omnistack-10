12f654baca7b396e2b04cb05843caac4647dbb38
Rafael <rafaelratacheski@gmail.com>
2020-01-15 22:51:34 -0300

    Criação Inicial da Aplicação Web

---
+++ web/src/App.js
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from 'react';
+
+import './global.css';
+import './App.css';
+import './Sidebar.css';
+import './Main.css';
+import api from './services/api.js';
+import DevItem from './components/DevItem';
+import DevForm from './components/DevForm';
+
+function App() {
+  const [devs, setDevs] = useState([]);
+
+  useEffect(() => {
+    async function loadDevs() {
+      const response = await api.get('/devs');
+      setDevs(response.data);
+    }
+
+    loadDevs();
+  }, []);
+
+  async function handleSubmit(data) {
+    const response = await api.post('/devs', data);
+    setDevs([...devs, response.data]);
+  }
+
+  return (
+    <div id="app">
+      <aside>
+        <strong>Cadastrar</strong>
+        <DevForm onSubmit={handleSubmit}/>
+      </aside>
+      <main>
+        <ul>
+          {devs.map(dev => (
+            <DevItem key={dev._id} dev={dev} />
+          ))}
+        </ul>
+      </main>
+    </div>
+  );
+}
+
+export default App;
