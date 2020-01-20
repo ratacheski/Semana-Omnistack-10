681a1023f8d659dbed16361cd6043df13af070c6
Rafael <rafaelratacheski@gmail.com>
2020-01-19 22:00:19 -0300

    Configuração de WebSocket da Aplicação

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -1,6 +1,7 @@
 const axios = require('axios');
 const Dev = require('../models/Dev');
 const parseStringAsArray = require('../utils/parseStringAsArray')
+const {findConnections, sendMessage} = require('../websocket')
 
 module.exports = {
     async store(req,res){
@@ -24,6 +25,16 @@ module.exports = {
                 techs: techsArray,
                 location
             });
+
+            //Filtrar as conexões que estão à no máximo 10 km de distância 
+            //e que possua pelo menos uma tecnologia do dev
+            const sendSocketMessageTo = findConnections(
+                {latitude, longitude},
+                techsArray
+            );
+
+            sendMessage(sendSocketMessageTo, 'new-dev', dev);
+
         }       
     
         return res.json(dev);
--- backend/src/index.js
+++ backend/src/index.js
@@ -2,8 +2,13 @@ const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes');
 const cors = require('cors');
+const http = require('http');
+const { setupWebSocket } = require('./websocket')
 
 const app = express();
+const server = http.Server(app);
+
+setupWebSocket(server);
 
 mongoose.connect('mongodb://rafael:33s2ftGG@cluster0-shard-00-00-dvnve.mongodb.net:27017,cluster0-shard-00-01-dvnve.mongodb.net:27017,cluster0-shard-00-02-dvnve.mongodb.net:27017/week10?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority', {
     useNewUrlParser: true,
@@ -14,4 +19,4 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333);
+server.listen(3333);
